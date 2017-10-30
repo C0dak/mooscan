7fa8a8f92bcf317b210822212ed501b699cc3ad7
James McLean <james.mclean@gmail.com>
2017-10-30 19:38:10 +1030

    Added a comment to explain the lack of code

--- mooscan.py
+++ mooscan.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 
+# This does look suspiciously empty, right?
+# Well, I have a lot of code I'd written, but it wasn't planned and was getting messy.
+# The solution? Refactor!
+# Also, develop with testing in mind (as I should have the first time) and for Python3
+
 
