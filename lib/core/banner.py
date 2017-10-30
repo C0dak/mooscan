06d692f1181b20cb2e905850b156dccd5a3ee25f
James McLean <james.mclean@gmail.com>
2017-10-30 20:58:53 +1030

    Minor banner fixes

--- lib/core/banner.py
+++ lib/core/banner.py
@@ -18,8 +18,8 @@ class banner(object):
 |   : '  |/     \   \  / \   \  /'--'.     /'   | '.'|/  /  ,.  ||   | |--'   
 ;   | |`-'       `----'   `----'   `--'---' |   :    ;  :   .'   |   |/       
 |   ;/                                       \   \  /|  ,     .-.'---'        
-'---'                                         `----'  `--`---'                                                                                                 
-MooScan - by vortex <james.mclean@gmail.com>.
+'---'                                         `----'  `--`---'                
+MooScan - by @vortexau 
 Please do not use this tool against environments you do not have permission to scan.
 The author will not be held responsible for any unauthorised usage of this software. 
 
